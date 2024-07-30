65471aa280e628ca73df52f5dae2d0e8965a297a
Patricio Schranz <faguzz.media@gmail.com>
2024-07-30 17:28:09 +0200

    working on home screen heading/countdown

--- src/js/create-dom-elements.js
+++ src/js/create-dom-elements.js
@@ -1,3 +1,72 @@
+// *********************
+// CREATE DOM ELEMENTS
+// *********************
+
+// .........................
+// SET HOME SCREEN HEADING
+//..........................
+
+// => callback => set the countdown display
+const countdownDisplay = (deadline, title)=>{
+
+    const rightNow = new Date().getTime()
+
+    let 
+        remainingTime = deadline > rightNow ? deadline - rightNow : 0,
+        remainingSeconds = (remainingTime / 1000) % 60,
+        remainingMinutes = (remainingTime / (1000 * 60) ) % 60,
+        remainingHours = (remainingTime / (1000 * 60 * 60) ) % 24,
+        remainingDays = (remainingTime / (1000 * 60 * 60 * 24) )
+
+    const setCountdown = setInterval(()=>{
+        remainingTime -= 1000
+       
+        if(remainingTime <= 0){
+            document.querySelector('.header_main-headline').innerHTML = `${title} is arrived`
+            clearInterval(setCountdown)
+        }
+        else{
+            remainingSeconds--
+
+            if(remainingSeconds <= 0){
+                remainingSeconds = 59
+                remainingMinutes--
+            }
+            if(remainingMinutes <= 0){
+                remainingMinutes = 59
+                remainingHours--
+            }
+            if(remainingHours <= 0){
+                remainingHours = 23
+                remainingDays--
+            }
+
+            document.querySelector('.header_main-headline').innerHTML = `
+                <span>${Math.floor(remainingDays).toString().padStart('2', '0')}</span> : 
+                <span>${Math.floor(remainingHours).toString().padStart('2', '0')}</span> : 
+                <span>${Math.floor(remainingMinutes).toString().padStart('2', '0')}</span> :
+                <span>${Math.floor(remainingSeconds).toString().padStart('2', '0')}</span>
+                `
+        }
+    }, 1000)
+ 
+}
+
+if(storageCountdownEvents.length <= 0){
+    document.querySelector('.header_main-headline').innerHTML = `CUSTOM COUNTDOWN`
+}
+else if(storageCountdownEvents.length === 1){
+    storageCountdownEvents[0].focus = true
+}
+
+countdownDisplay(storageCountdownEvents[0].deadline, storageCountdownEvents[0].title)
+
+
+
+// .........................
+// FILL THE CUSTOM DATALIST
+//..........................
+
 const countDownTitles = []
 
 storageCountdownEvents.forEach(countdown =>{
